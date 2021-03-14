2cefb20f1ea00ccabe43b9cee26968310b884e63
netusb <51571969+netusb@users.noreply.github.com>
2021-03-14 23:50:46 +0800

    Update one.patch.sh

--- patch/one.patch.sh
+++ patch/one.patch.sh
@@ -1,21 +1,20 @@
-cp -r /home/runner/openmptcprouter/patch/image-commands.mk /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/include/image-commands.mk
-
-cp -r /home/runner/openmptcprouter/patch/02_network /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/etc/board.d/02_network
-
-cp -r /home/runner/openmptcprouter/patch/11-ath10k-caldata /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/etc/hotplug.d/firmware/11-ath10k-caldata
-
-cp -r /home/runner/openmptcprouter/patch/05_set_iface_mac_ipq806x.sh /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/lib/preinit/05_set_iface_mac_ipq806x.sh
- 
-cp -r /home/runner/openmptcprouter/patch/platform.sh /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/lib/upgrade/platform.sh
-
-cp -r /home/runner/openmptcprouter/patch/ruijie.sh /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/lib/upgrade/ruijie.sh
-
-cp -r /home/runner/openmptcprouter/patch/qcom-ipq8064-rg-mtfi-m520.dts /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/files/arch/arm/boot/dts/qcom-ipq8064-rg-mtfi-m520.dts
-
-cp -r /home/runner/openmptcprouter/patch/target-linux-ipq806x-image/Makefile  /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/image/Makefile
-
-cp -r /home/runner/openmptcprouter/patch/Makefile /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/Makefile
-
-cp -r /home/runner/openmptcprouter/patch/0069-arm-boot-add-dts-files.patch /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/patches-5.4/0069-arm-boot-add-dts-files.patch
-
-cp -r /home/runner/openmptcprouter/patch/config-omr-m520 /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/.config
+#!/bin/sh
+cp -r /home/runner/patch/image-commands.mk /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/include/image-commands.mk
+sleep 1
+cp -r /home/runner/patch/02_network /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/etc/board.d/02_network
+sleep 1
+cp -r /home/runner/patch/11-ath10k-caldata /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/etc/hotplug.d/firmware/11-ath10k-caldata
+sleep 1
+cp -r /home/runner/patch/05_set_iface_mac_ipq806x.sh /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/lib/preinit/05_set_iface_mac_ipq806x.sh
+sleep 1
+cp -r /home/runner/patch/platform.sh /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/lib/upgrade/platform.sh
+sleep 1
+cp -r /home/runner/patch/ruijie.sh /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/base-files/lib/upgrade/ruijie.sh
+sleep 1
+cp -r /home/runner/patch/qcom-ipq8064-rg-mtfi-m520.dts /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/files/arch/arm/boot/dts/qcom-ipq8064-rg-mtfi-m520.dts
+sleep 1
+cp -r /home/runner/patch/target-linux-ipq806x-image/Makefile  /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/image/Makefile
+sleep 1
+cp -r /home/runner/patch/Makefile /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/Makefile
+sleep 1
+cp -r /home/runner/patch/0069-arm-boot-add-dts-files.patch /home/runner/openmptcprouter/arm_cortex-a15_neon-vfpv4/source/target/linux/ipq806x/patches-5.4/0069-arm-boot-add-dts-files.patch
